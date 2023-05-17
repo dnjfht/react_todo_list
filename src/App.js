578a5c7a907d7538fe776ba3893257aac2be993b
Yoo Seungmin <dnjfht@gmail.com>
2023-05-18 01:43:42 +0900

    to do list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import styles from "./App.module.css";
+import { TiDelete } from "react-icons/ti";
+import { BsFillPlusCircleFill } from "react-icons/bs";
 
 function App() {
+  const [appear, setAppear] = useState(false);
+
+  console.log(appear);
+
   return (
     <div className={styles.Wrap}>
-      <div className={styles.Box}></div>
+      <div className={styles.Box}>
+        <button className={styles.InputAppearBtn}>
+          {appear ? (
+            <TiDelete style={{ fontSize: 80, color: "#ff6b6b" }} />
+          ) : (
+            <BsFillPlusCircleFill style={{ fontSize: 80, color: "#38d9a9" }} />
+          )}
+        </button>
+      </div>
     </div>
   );
 }
