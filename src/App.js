cdac94d8d9c8fdf0b68fde378f446b557ca2c968
Yoo Seungmin <dnjfht@gmail.com>
2023-05-18 02:00:21 +0900

    to do list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styles from "./App.module.css";
-import { TiDelete } from "react-icons/ti";
-import { BsFillPlusCircleFill } from "react-icons/bs";
+import { BsFillPlusCircleFill, BsFillXCircleFill } from "react-icons/bs";
+import InputWrap from "./components/InputWrap";
 
 function App() {
   const [appear, setAppear] = useState(false);
@@ -10,15 +10,20 @@ function App() {
 
   return (
     <div className={styles.Wrap}>
-      <div className={styles.Box}>
-        <button className={styles.InputAppearBtn}>
-          {appear ? (
-            <TiDelete style={{ fontSize: 80, color: "#ff6b6b" }} />
-          ) : (
-            <BsFillPlusCircleFill style={{ fontSize: 80, color: "#38d9a9" }} />
-          )}
-        </button>
-      </div>
+      <div className={styles.Box}>{appear && <InputWrap />}</div>
+
+      <button
+        onClick={() => {
+          setAppear((prev) => !prev);
+        }}
+        className={styles.InputAppearBtn}
+      >
+        {appear ? (
+          <BsFillXCircleFill style={{ fontSize: 80, color: "#ff6b6b" }} />
+        ) : (
+          <BsFillPlusCircleFill style={{ fontSize: 80, color: "#38d9a9" }} />
+        )}
+      </button>
     </div>
   );
 }
