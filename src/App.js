2837be36038b2052f31a61895d1b4240121da9fd
Yoo Seungmin <dnjfht@gmail.com>
2023-05-18 01:06:11 +0900

    css 변수 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import styles from "./App.module.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className={styles.Wrap}>
+      <div className={styles.Box}></div>
+    </div>
+  );
 }
 
 export default App;
