3d91127091e6ba8a08c5fbbc1087e40da1d7f17a
Yoo Seungmin <dnjfht@gmail.com>
2023-05-18 05:53:43 +0900

    TODOLIST

--- src/components/InputWrap.js
+++ src/components/InputWrap.js
@@ -1,5 +1,15 @@
 import React from "react";
+import styles from "../App.module.css";
 
 export default function InputWrap() {
-  return <div>InputWrap</div>;
+  return (
+    <div className={styles.InputWrap}>
+      <input
+        className={styles.InputTodo}
+        type="text"
+        placeholder="Write Todo..."
+      />
+      <button className={styles.SubmitBtn}>Add</button>
+    </div>
+  );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,42 @@
 import React from "react";
+import styles from "../App.module.css";
+import { ImQuill } from "react-icons/im";
 
 export default function Todo({ filterTodo }) {
   return (
-    <div key={filterTodo.id}>
-      <h2>{filterTodo.date}</h2>
-      <p>{filterTodo.day}</p>
-      <p>{filterTodo.title}</p>
+    <div key={filterTodo.id} className={styles.Todo}>
+      <ImQuill
+        style={{
+          color: "var(--color-accent)",
+          fontSize: 26,
+          position: "absolute",
+          top: -3,
+          left: -2,
+        }}
+      />
+      <h2 style={{ margin: 0, color: "var(--color-text)" }}>
+        {filterTodo.date}
+      </h2>
+      <p
+        style={{
+          margin: 0,
+          marginTop: 4,
+          fontSize: 14,
+          color: "var(--color-gray)",
+        }}
+      >
+        {filterTodo.day}
+      </p>
+      <p
+        style={{
+          margin: 0,
+          marginTop: 12,
+          fontSize: 17,
+          color: "var(--color-text)",
+        }}
+      >
+        {filterTodo.title}
+      </p>
     </div>
   );
 }
